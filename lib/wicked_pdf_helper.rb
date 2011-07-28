c74c1e37cc9cd1319d9a9b885fa4e4b14d2a6927
Maria Jesus Lobo <mjlobo@redvel.cl>
2011-07-28 12:56:04 -0400

    wicked_pdf modified for rails 3.1

--- lib/wicked_pdf_helper.rb
+++ lib/wicked_pdf_helper.rb
@@ -1,17 +1,17 @@
 module WickedPdfHelper
   def wicked_pdf_stylesheet_link_tag(*sources)
-    css_dir = Rails.root.join('public','stylesheets')
+    css_dir = Rails.root.join('app','assets','stylesheets')
     sources.collect { |source|
       "<style type='text/css'>#{File.read(css_dir.join(source+'.css'))}</style>"
     }.join("\n").html_safe
   end
 
   def wicked_pdf_image_tag(img, options={})
-    image_tag "file://#{Rails.root.join('public', 'images', img)}", options
+    image_tag "file://#{Rails.root.join('app','assets', 'images', img)}", options
   end
 
   def wicked_pdf_javascript_src_tag(jsfile, options={})
-    javascript_src_tag "file://#{Rails.root.join('public','javascripts',jsfile)}", options
+    javascript_src_tag "file://#{Rails.root.join('app','assets','javascripts',jsfile)}", options
   end
 
   def wicked_pdf_javascript_include_tag(*sources)
